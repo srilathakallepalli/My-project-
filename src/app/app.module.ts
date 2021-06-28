d0b14aec587f0e53dd62e30ac955da984d21738b
srilathakallepalli <kallepallisrilatha7@gmail.com>
2021-06-28 11:35:11 +0530

    my commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BodyComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
